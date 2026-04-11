5cceeaf11ce657591271e946a0fb50184ac778c8
JC FIESTADA <johnchristianfiestada69@gmail.com>
2026-04-11 21:50:41 +0800

    tried to fix the iframe element and the download issue on mobile, but I just learned that iframe doesnt support pdf rendering in mobile browsers. still pushing this since it’s better than the previous version anyway

--- client/src/scripts/pages/preview.ts
+++ client/src/scripts/pages/preview.ts
@@ -1,35 +1,42 @@
 import { IdResponse } from "../interface/id.response";
 import { showToast } from "../ui/toast";
+import { saveAs } from 'file-saver';
 
 const pdfViewer = document.getElementById("pdfViewer") as HTMLIFrameElement;
 const downloadBtn = document.getElementById("download-btn") as HTMLButtonElement;
 
 document.addEventListener("DOMContentLoaded", () => {
     const rawData = sessionStorage.getItem("result");
-    if (rawData === null){
+    
+    if (rawData === null) {
         window.location.href = "./main.html";
         return;
     }
 
-    const data : IdResponse = JSON.parse(rawData);
+    const data: IdResponse = JSON.parse(rawData);
 
     if (data.pdfStatusCode !== 200) {
         window.location.href = "./main.html";
         return;
     }
 
-    pdfViewer.src = `data:application/pdf;base64,${data.pdfBase64}`
+    const binaryString = atob(data.pdfBase64);
+    const bytes = new Uint8Array(binaryString.length);
+    for (let i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    const pdfBlob = new Blob([bytes], { type: 'application/pdf' });
+
+    const blobUrl = URL.createObjectURL(pdfBlob);
+    pdfViewer.src = blobUrl;
 
     downloadBtn?.addEventListener("click", () => {
-        const link = document.createElement('a');
-        link.href = `data:application/pdf;base64,${data.pdfBase64}`;
-        link.download = "id-card.pdf";
-        link.click();
+        saveAs(pdfBlob, "id-card.pdf");
     });
 
     showToast({
         type: "success",
-        message : "Id has been successfully generated",
-        duration : 3000
+        message: "Id has been successfully generated",
+        duration: 3000
     });
 });
